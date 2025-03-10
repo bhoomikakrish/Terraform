fc35493b8bc5d608187f0c393f88ad1e9818fddb
bhoomikakrish <121486650+bhoomikakrish@users.noreply.github.com>
2025-03-10 13:07:29 +0530

    Create main.tf

---
+++ day-2/modules/ec2/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = var.region_value
+    }
+resource "aws_instance" "Demo-instance" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value 
+}
