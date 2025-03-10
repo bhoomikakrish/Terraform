5e8d5f58b0d158058b84b7ac54c1a7818e3259de
bhoomikakrish <121486650+bhoomikakrish@users.noreply.github.com>
2025-03-10 13:08:00 +0530

    Create variables.tf

---
+++ day-2/modules/ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "region_value" {
+    description = "value for region_value"  
+}
+variable "ami_value" {
+    description = "value for ami_value"  
+}
+variable "instance_type_value" {
+    description = "value of instance_type_value"  
+}
+variable "subnet_id_value" {
+    description = "value of subnet_id_value"  
+}
